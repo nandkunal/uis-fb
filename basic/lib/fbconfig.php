b6b393aa47b380a6c63e52ae92348ed17fec328d
Kunal Khaware <nandkunal@gmail.com>
2014-08-09 06:49:53 +0530

    Updated fbconfig.php file
    Removed the api secret and app Id

--- basic/lib/fbconfig.php
+++ basic/lib/fbconfig.php
@@ -1,7 +1,7 @@
 <?php
 //Facebook Application Configuration.
-$facebook_appid='424666134275455';
-$facebook_app_secret='5a3e377847783aedf0d95991af7fd930';
+$facebook_appid='******************';
+$facebook_app_secret='*********************';
 
 $facebook = new Facebook(array(
 'appId'  => $facebook_appid,
